b5249d9fc9f558d4bef58f7868de72542e6d5962
Christine Contreras <christineec23@yahoo.com>
2021-10-07 15:33:58 -0500

    working

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,15 +1,46 @@
 class CheesesController < ApplicationController
 
   # GET /cheeses
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses
+  # end
+  # def index
+  #   cheeses = Cheese.all
+  #   render json: cheeses, only: [:id, :name, :price, :is_best_seller]
+  # end 
   def index
     cheeses = Cheese.all
-    render json: cheeses
-  end
+    render json: cheeses, except: [:created_at, :updated_at]
+  end 
+  
 
   # GET /cheeses/:id
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese
+  # end
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: {
+  #     id: cheese.id,
+  #     name: cheese.name,
+  #     price: cheese.price,
+  #     is_best_seller: cheese.is_best_seller
+  #   }
+  # end 
+  # def show
+  #   cheese = Cheese.find_by(id: params[:id])
+  #   render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+  # end 
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
-  end
+    if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' }, status: :not_found
+    end
+  end 
+
 
 end
